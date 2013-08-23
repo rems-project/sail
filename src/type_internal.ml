30b2d09f92f57f78d47edae54bf3f75bc34a7238
Kathy Gray <kathryn.gray@cl.cam.ac.uk>
2013-08-23 11:54:37 +0100

    Add built-in type bit

--- src/type_internal.ml
+++ src/type_internal.ml
@@ -69,6 +69,7 @@ let initial_kind_env =
     ("bool", {k = K_Typ});
     ("nat", {k = K_Typ});
     ("unit", {k = K_Typ});
+    ("bit", {k = K_Typ});
     ("list", {k = K_Lam( [{k = K_Typ}], {k = K_Typ})});
     ("enum", {k = K_Lam( [ {k = K_Nat}; {k= K_Nat}; {k=K_Ord} ], {k = K_Typ}) });
     ("vector", {k = K_Lam( [ {k = K_Nat}; {k = K_Nat}; {k= K_Ord} ; {k=K_Typ}], {k=K_Typ}) } )
