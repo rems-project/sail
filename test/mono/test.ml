71f2f7875235621ce155c8c7d7326fabdbc63b35
Brian Campbell <Brian.Campbell@ed.ac.uk>
2018-04-17 17:52:37 +0100

    Enable mono builtins test, tweak test output

--- test/mono/test.ml
+++ test/mono/test.ml
@@ -1,3 +1,5 @@
 match Out.run() with
 | Done _ -> exit 0
-| _ -> exit 1
+| Fail s -> prerr_endline ("Fail: " ^ s); exit 1
+| Error s -> prerr_endline ("Error: " ^ s); exit 1
+| _ -> prerr_endline "Unexpected outcome"; exit 1
