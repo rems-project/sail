dda8dd8689ea4756c41a5bb36a123b564dfb0b12
Brian Campbell <Brian.Campbell@ed.ac.uk>
2018-06-07 16:20:21 +0100

    Coq: track add_typquant change

--- src/pretty_print_coq.ml
+++ src/pretty_print_coq.ml
@@ -1464,7 +1464,7 @@ let doc_regtype_fields (tname, (n1, n2, fields)) =
 
 (* Remove some type variables in a similar fashion to merge_kids_atoms *)
 let doc_axiom_typschm typ_env (TypSchm_aux (TypSchm_ts (tqs,typ),l) as ts) =
-  let typ_env = add_typquant tqs typ_env in
+  let typ_env = add_typquant l tqs typ_env in
   match typ with
   | Typ_aux (Typ_fn (args_ty, ret_ty, eff),l') ->
      let check_typ (args,used) typ =
