5060f163327fe0955a2deee26635e243a12d4f56
Alasdair <alasdair.armstrong@gmail.com>
2019-08-19 21:30:39 +0100

    Move guard removal rewrite to pattern_rewrites file

--- src/pattern_rewrites.ml
+++ src/pattern_rewrites.ml
@@ -73,6 +73,112 @@ let rec irrefutable (P_aux (aux, annot)) =
   | P_vector pats | P_vector_concat pats | P_list pats | P_tup pats -> List.for_all irrefutable pats
   | P_or _ | P_not _ -> Reporting.unreachable (fst annot) __POS__ "Or or not pattern found in replace_views"
 
+(* Check if one pattern subsumes the other, and if so, calculate a
+   substitution of variables that are used in the same position.
+   TODO: Check somewhere that there are no variable clashes (the same variable
+   name used in different positions of the patterns)
+ *)
+let rec subsumes_pat (P_aux (p1,annot1) as pat1) (P_aux (p2,annot2) as pat2) =
+  let open Type_check in
+  let rewrap p = P_aux (p,annot1) in
+  let subsumes_list s pats1 pats2 =
+    if List.length pats1 = List.length pats2
+    then
+      let subs = List.map2 s pats1 pats2 in
+      List.fold_right
+        (fun p acc -> match p, acc with
+          | Some subst, Some substs -> Some (subst @ substs)
+          | _ -> None)
+        subs (Some [])
+    else None in
+  match p1, p2 with
+  | P_lit (L_aux (lit1,_)), P_lit (L_aux (lit2,_)) ->
+      if lit1 = lit2 then Some [] else None
+  | P_or(pat1, pat2), _ -> (* todo: possibly not the right answer *) None
+  | _, P_or(pat1, pat2) -> (* todo: possibly not the right answer *) None
+  | P_not(pat), _ -> (* todo: possibly not the right answer *) None
+  | _, P_not(pat) -> (* todo: possibly not the right answer *) None
+  | P_as (pat1,_), _ -> subsumes_pat pat1 pat2
+  | _, P_as (pat2,_) -> subsumes_pat pat1 pat2
+  | P_typ (_,pat1), _ -> subsumes_pat pat1 pat2
+  | _, P_typ (_,pat2) -> subsumes_pat pat1 pat2
+  | P_id (Id_aux (id1,_) as aid1), P_id (Id_aux (id2,_) as aid2) ->
+    if id1 = id2 then Some []
+    else if Env.lookup_id aid1 (env_of_annot annot1) = Unbound
+    then if Env.lookup_id aid2 (env_of_annot annot2) = Unbound
+      then Some [(id2,id1)]
+      else Some []
+    else None
+  | P_id id1, _ ->
+    if Env.lookup_id id1 (env_of_annot annot1) = Unbound then Some [] else None
+  | P_var (pat1,_), P_var (pat2,_) -> subsumes_pat pat1 pat2
+  | P_wild, _ -> Some []
+  | P_app (Id_aux (id1,l1),args1), P_app (Id_aux (id2,_),args2) ->
+    if id1 = id2 then subsumes_list subsumes_pat args1 args2 else None
+  | P_vector pats1, P_vector pats2
+  | P_vector_concat pats1, P_vector_concat pats2
+  | P_tup pats1, P_tup pats2
+  | P_list pats1, P_list pats2 ->
+    subsumes_list subsumes_pat pats1 pats2
+  | P_list (pat1 :: pats1), P_cons _ ->
+    subsumes_pat (rewrap (P_cons (pat1, rewrap (P_list pats1)))) pat2
+  | P_cons _, P_list (pat2 :: pats2)->
+    subsumes_pat pat1 (rewrap (P_cons (pat2, rewrap (P_list pats2))))
+  | P_cons (pat1, pats1), P_cons (pat2, pats2) ->
+    (match subsumes_pat pat1 pat2, subsumes_pat pats1 pats2 with
+    | Some substs1, Some substs2 -> Some (substs1 @ substs2)
+    | _ -> None)
+  | _, P_wild -> if irrefutable pat1 then Some [] else None
+  | _ -> None
+
+let id_is_unbound id env =
+  match Type_check.Env.lookup_id id env with
+  | Unbound -> true
+  | _ -> false
+       
+(* A simple check for pattern disjointness; used for optimisation in the
+   guarded pattern rewrite step *)
+let rec disjoint_pat env (P_aux (p1,annot1) as pat1) (P_aux (p2,annot2) as pat2) =
+  match p1, p2 with
+  | P_as (pat1, _), _ -> disjoint_pat env pat1 pat2
+  | _, P_as (pat2, _) -> disjoint_pat env pat1 pat2
+  | P_typ (_, pat1), _ -> disjoint_pat env pat1 pat2
+  | _, P_typ (_, pat2) -> disjoint_pat env pat1 pat2
+  | P_var (pat1, _), _ -> disjoint_pat env pat1 pat2
+  | _, P_var (pat2, _) -> disjoint_pat env pat1 pat2
+  | P_id id, _ when id_is_unbound id env -> false
+  | _, P_id id when id_is_unbound id env -> false
+  | P_id id1, P_id id2 -> Id.compare id1 id2 <> 0
+  | P_app (id1, args1), P_app (id2, args2) ->
+     Id.compare id1 id2 <> 0 || List.exists2 (disjoint_pat env) args1 args2
+  | P_vector pats1, P_vector pats2
+  | P_tup pats1, P_tup pats2
+  | P_list pats1, P_list pats2 ->
+     List.exists2 (disjoint_pat env) pats1 pats2
+  | _ -> false
+
+let equiv_pats pat1 pat2 =
+  match subsumes_pat pat1 pat2, subsumes_pat pat2 pat1 with
+  | Some _, Some _ -> true
+  | _, _ -> false
+
+let subst_id_pat pat (id1,id2) =
+  let p_id (Id_aux (id,l)) = (if id = id1 then P_id (Id_aux (id2,l)) else P_id (Id_aux (id,l))) in
+  fold_pat { id_algebra with p_id = p_id } pat
+
+let subst_id_exp exp (id1,id2) =
+  Ast_util.subst (Id_aux (id1, Parse_ast.Unknown))
+    (E_aux (E_id (Id_aux (id2, Parse_ast.Unknown)), (Parse_ast.Unknown, Type_check.empty_tannot)))
+    exp
+
+let remove_wildcards pre (P_aux (_,(l,_)) as pat) =
+  fold_pat
+    { id_algebra with
+      p_aux = function
+              | (P_wild,(l,annot)) -> P_aux (P_id (mk_id "w__0") ,(l,annot))
+              | (p,annot) -> P_aux (p,annot) }
+    pat
+  
 (**************************************************************************)
 (* 1. Pattern rewrites                                                    *)
 (**************************************************************************)
@@ -384,3 +490,151 @@ let rewrite_def = function
   | def -> def
 
 let swap_guards (Defs defs) = Defs (List.map rewrite_def defs)
+
+let get_loc_exp (E_aux (_,(l,_))) = l
+
+let annot_exp_effect e_aux l env typ effect = E_aux (e_aux, (l, Type_check.mk_tannot env typ effect))
+let annot_exp e_aux l env typ = annot_exp_effect e_aux l env typ no_effect
+                            
+let rec pat_to_exp ((P_aux (pat,(l,annot))) as p_aux) =
+  let open Type_check in
+  let rewrap e = E_aux (e,(l,annot)) in
+  let env = env_of_pat p_aux in
+  let typ = typ_of_pat p_aux in
+  match pat with
+  | P_lit lit -> rewrap (E_lit lit)
+  | P_wild -> raise (Reporting.err_unreachable l __POS__
+      "pat_to_exp given wildcard pattern")
+  | P_or(pat1, pat2) -> (* todo: insert boolean or *) pat_to_exp pat1 
+  | P_not(pat) -> (* todo: insert boolean not *) pat_to_exp pat
+  | P_as (pat,id) -> rewrap (E_id id)
+  | P_var (pat, _) -> pat_to_exp pat
+  | P_typ (_,pat) -> pat_to_exp pat
+  | P_id id -> rewrap (E_id id)
+  | P_app (id,pats) -> rewrap (E_app (id, List.map pat_to_exp pats))
+  | P_vector pats -> rewrap (E_vector (List.map pat_to_exp pats))
+  | P_vector_concat pats -> begin
+      let empty_vec = E_aux (E_vector [], (l,())) in
+      let concat_vectors vec1 vec2 =
+        E_aux (E_vector_append (vec1, vec2), (l, ()))
+      in
+      check_exp env (List.fold_right concat_vectors (List.map (fun p -> strip_exp (pat_to_exp p)) pats) empty_vec) typ
+    end
+  | P_tup pats -> rewrap (E_tuple (List.map pat_to_exp pats))
+  | P_list pats -> rewrap (E_list (List.map pat_to_exp pats))
+  | P_cons (p,ps) -> rewrap (E_cons (pat_to_exp p, pat_to_exp ps))
+  | P_string_append (pats) -> begin
+      let empty_string = annot_exp (E_lit (L_aux (L_string "", l))) l env string_typ in
+      let string_append str1 str2 =
+        annot_exp (E_app (mk_id "string_append", [str1; str2])) l env string_typ
+      in
+      (List.fold_right string_append (List.map pat_to_exp pats) empty_string)
+    end
+                            
+let case_exp e t cs =
+  let open Type_check in
+  let l = get_loc_exp e in
+  let env = env_of e in
+  let annot = (get_loc_exp e, Some (env_of e, t, no_effect)) in
+  match cs with
+  | [(P_aux (P_wild, _), body, _)] ->
+     fix_eff_exp body
+  | [(P_aux (P_id id, pannot) as pat, body, _)] ->
+     fix_eff_exp (annot_exp (E_let (LB_aux (LB_val (pat, e), pannot), body)) l env t)
+  | _ ->
+     let pexp (pat,body,annot) = Pat_aux (Pat_case (pat,[],body),annot) in
+     let ps = List.map pexp cs in
+     fix_eff_exp (annot_exp (E_case (e, ps)) l env t)
+                            
+(* Rewrite guarded patterns into a combination of if-expressions and
+   unguarded pattern matches
+
+   Strategy:
+   - Split clauses into groups where the first pattern subsumes all the
+     following ones
+   - Translate the groups in reverse order, using the next group as a
+     fall-through target, if there is one
+   - Within a group,
+     - translate the sequence of clauses to an if-then-else cascade using the
+       guards as long as the patterns are equivalent modulo substitution, or
+     - recursively translate the remaining clauses to a pattern match if
+       there is a difference in the patterns.
+
+  TODO: Compare this more closely with the algorithm in the CPP'18 paper of
+  Spector-Zabusky et al, who seem to use the opposite grouping and merging
+  strategy to ours: group *mutually exclusive* clauses, and try to merge them
+  into a pattern match first instead of an if-then-else cascade.
+*)
+let rewrite_guarded_clauses l env pat_typ typ cs =
+  let open Type_check in
+  let rec group fallthrough clauses =
+    let add_clause (pat,cls,annot) c = (pat,cls @ [c],annot) in
+    let rec group_aux current acc = (function
+      | ((pat,guard,body,annot) as c) :: cs ->
+          let (current_pat,_,_) = current in
+          (match subsumes_pat current_pat pat with
+            | Some substs ->
+                let pat' = List.fold_left subst_id_pat pat substs in
+                let guard' = (match guard with
+                  | [G_aux (G_if exp, l)] -> [G_aux (G_if (List.fold_left subst_id_exp exp substs), l)]
+                  | _ -> []) in
+                let body' = List.fold_left subst_id_exp body substs in
+                let c' = (pat',guard',body',annot) in
+                group_aux (add_clause current c') acc cs
+            | None ->
+                let pat = match cs with _::_ -> remove_wildcards "g__" pat | _ -> pat in
+                group_aux (pat,[c],annot) (acc @ [current]) cs)
+      | [] -> acc @ [current]) in
+    let groups = match clauses with
+      | [(pat,guard,body,annot) as c] ->
+          [(pat, [c], annot)]
+      | ((pat,guard,body,annot) as c) :: cs ->
+          group_aux (remove_wildcards "g__" pat, [c], annot) [] cs
+      | _ ->
+          raise (Reporting.err_unreachable l __POS__
+            "group given empty list in rewrite_guarded_clauses") in
+    let add_group cs groups = (if_pexp (groups @ fallthrough) cs) :: groups in
+    List.fold_right add_group groups []
+  and if_pexp fallthrough (pat,cs,annot) = (match cs with
+    | c :: _ ->
+        let body = if_exp fallthrough pat cs in
+        (pat, body, annot)
+    | [] ->
+        raise (Reporting.err_unreachable l __POS__
+            "if_pexp given empty list in rewrite_guarded_clauses"))
+  and if_exp fallthrough current_pat = (function
+    | (pat,guard,body,l) :: ((pat',guard',body',l') as c') :: cs ->
+        (match guard with
+          | [G_aux (G_if exp, l)] ->
+              let else_exp =
+                if equiv_pats current_pat pat'
+                then if_exp fallthrough current_pat (c' :: cs)
+                else case_exp (pat_to_exp current_pat) (typ_of body') (group fallthrough (c' :: cs)) in
+              fix_eff_exp (annot_exp (E_if (exp,body,else_exp)) l (env_of exp) (typ_of body))
+          | _ -> body)
+    | [(pat,guard,body,l)] ->
+        (* For singleton clauses with a guard, use fallthrough clauses if the
+           guard is not satisfied, but only those fallthrough clauses that are
+           not disjoint with the current pattern *)
+        let overlapping_clause (pat, _, _) = not (disjoint_pat env current_pat pat) in
+        let fallthrough = List.filter overlapping_clause fallthrough in
+        (match guard, fallthrough with
+          | [G_aux (G_if exp, l)], _ :: _ ->
+              let else_exp = case_exp (pat_to_exp current_pat) (typ_of body) fallthrough in
+              fix_eff_exp (annot_exp (E_if (exp,body,else_exp)) l (env_of exp) (typ_of body))
+          | _, _ -> body)
+    | [] ->
+        raise (Reporting.err_unreachable l __POS__
+            "if_exp given empty list in rewrite_guarded_clauses")) in
+  let is_complete = Pattern_completeness.is_complete (Env.pattern_completeness_ctx env) (List.map construct_pexp cs) in
+  let fallthrough =
+    if not is_complete then
+      let p = P_aux (P_wild, (gen_loc l, mk_tannot env pat_typ no_effect)) in
+      let msg = "Pattern match failure at " ^ Reporting.short_loc_to_string l in
+      let a = mk_exp ~loc:(gen_loc l) (E_assert (mk_lit_exp L_false, mk_lit_exp (L_string msg))) in
+      let b = mk_exp ~loc:(gen_loc l) (E_exit (mk_lit_exp L_unit)) in
+      let e = check_exp env (mk_exp ~loc:(gen_loc l) (E_block [a; b])) typ in
+      [(p,[],e,l)]
+    else []
+  in
+  group [] (cs @ fallthrough)
