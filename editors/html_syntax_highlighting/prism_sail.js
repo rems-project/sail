31a65c9b7383d2a87da0fbcf5c265d533146ac23
jp <jp622@cam.ac.uk>
2020-02-12 17:46:43 +0000

    improve syntax highlighting

--- editors/html_syntax_highlighting/prism_sail.js
+++ editors/html_syntax_highlighting/prism_sail.js
@@ -15,19 +15,17 @@ Prism.languages.sail = {
 	  pattern: /(["])(?:\1\1|\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 	  greedy: true
       },
-      'operator': /:|->|==|!=|{\||\|}|=>|=|\+|-|@|\(\)|\(|\)|_|;|&|~|\.\.|,|\^|\*/,
+      'operator': /:|->|==|!=|{\||\|}|=>|=|\+|-|@|\(\)|\(|\)|_|;|&|~|\.\.|,|\^|\*|\|/,
       'keyword': [
 	  {
-	      pattern: /(^|[^.])\b(type|union|register|function|clause|scattered|val|effect|end|true|false|let|if|then)\b/,
+	      pattern: /(^|[^.])\b(type|union|register|function|clause|scattered|val|effect|end|true|false|let|if|then|else|match|as)\b/,
 	      lookbehind: true
 	  }
       ],
-      //'builtin': /\b(?:fx|fy|xf[xy]?|yfx?)\b/,
       'builtin': /\b[A-Z][A-Za-z_\-\/0-9']*/,
-      'variable': /\b'?[a-z_][A-Za-z_\-\/0-9']*/,
-      //'function': /\b[a-z_]\w*(?:(?=\()|\/\d+)/,
+      'variable': /\b[a-z_][A-Za-z_\-\/0-9']*/,
+      'function': /\b'[a-z_][A-Za-z_\-\/0-9']*/,
       'number': /\b0b\d+|\b\d+\.?\d*/,
-      // Custom operators are allowed
       'punctuation': /[(){}\[\],:]/
 };
 
