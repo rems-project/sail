550db27f8c27e03ecc4defc804c8f360af64fbd6
Thomas Bauereiss <tb592@cl.cam.ac.uk>
2020-03-27 18:06:26 +0000

    Update path for newer versions of BBV Coq library

--- test/coq/run_tests.sh
+++ test/coq/run_tests.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SAILDIR="$DIR/../.."
 TYPECHECKTESTSDIR="$DIR/../typecheck/pass"
 EXTRATESTSDIR="$DIR/pass"
-BBVDIR="$DIR/../../../bbv/theories"
+BBVDIR="$DIR/../../../bbv/src/bbv"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
