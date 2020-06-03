dc4fe34905891510b7fd7d3b9fb86af49ce89879
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2020-06-03 14:36:00 +0100

    Add a workflow for latest released ocaml

---
+++ etc/ci_opam_latest_build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+opam init -y --no-setup --compiler=4.10 --shell=sh
+
+eval `opam config env`
+
+opam repository -y add rems https://github.com/rems-project/opam-repository.git
+opam pin -y add sail .
+opam install -y -v sail
+sail -v
