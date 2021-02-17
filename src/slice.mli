bf2ed594bdcdb3917116749ccd316cfd9364ebd9
Thomas Sewell <tsewell.inbox@gmail.com>
2021-02-17 19:06:18 +0000

    Slice AST before generating SMT props
    Filtering the AST to nodes reachable from the SMT properties
    speeds up the process substantially

--- src/slice.mli
+++ src/slice.mli
@@ -69,3 +69,5 @@ val graph_of_ast : Type_check.tannot defs -> Graph.Make(Node).graph
 val dot_of_ast : out_channel -> Type_check.tannot defs -> unit
 
 val filter_ast : Set.Make(Node).t -> Graph.Make(Node).graph -> Type_check.tannot defs -> Type_check.tannot defs
+
+val filter_ast_ids : IdSet.t -> IdSet.t -> Type_check.tannot defs -> Type_check.tannot defs
