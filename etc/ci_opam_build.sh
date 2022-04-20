86c586f58e9b28f7bd9ee18a7bfaea2b14350da7
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2022-04-20 15:19:13 +0100

    Pin all packages in CI scripts

--- etc/ci_opam_build.sh
+++ etc/ci_opam_build.sh
@@ -7,6 +7,6 @@ opam init -y --no-setup --compiler=4.07.0 --shell=sh
 eval `opam config env`
 
 opam repository -y add rems https://github.com/rems-project/opam-repository.git
-opam pin -y add sail .
+opam pin -y add .
 opam install -y -v sail
 sail -v
