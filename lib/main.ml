36f70e52b7386dde04d6141263639babf4ea3434
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2023-05-03 03:59:50 +0100

    Revert autoformatting to ml files in lib/

--- lib/main.ml
+++ lib/main.ml
@@ -65,32 +65,25 @@
 (*  SUCH DAMAGE.                                                            *)
 (****************************************************************************)
 
-open Elf_loader
+open Elf_loader;;
 
 let opt_file_arguments = ref ([] : string list)
-let opt_raw_files = ref ([] : (string * Nat_big_num.num) list)
-
-let options =
-  Arg.align
-    [
-      ( "-raw",
-        Arg.String
-          (fun s ->
-            let l = Util.split_on_char '@' s in
-            let file, addr =
-              match l with
-              | [fname; addr] -> (fname, Nat_big_num.of_string addr)
-              | _ -> raise (Arg.Bad (s ^ " not of form <filename>@<addr>"))
-            in
-            opt_raw_files := (file, addr) :: !opt_raw_files
-          ),
-        "<file@0xADDR> load a raw binary in memory at given address."
-      );
-      ("-cycle-limit", Arg.Set_int Sail_lib.opt_cycle_limit, "<int> exit after given number of instructions executed.");
-    ]
+let opt_raw_files = ref ([] : (string * Nat_big_num.num)  list)
+let options = Arg.align [
+    ( "-raw",
+      Arg.String (fun s ->
+      let l = Util.split_on_char '@' s in
+      let (file, addr) = match l with
+        | [fname;addr] -> (fname, Nat_big_num.of_string addr)
+        | _ -> raise (Arg.Bad (s ^ " not of form <filename>@<addr>")) in
+      opt_raw_files := (file, addr) :: !opt_raw_files),
+      "<file@0xADDR> load a raw binary in memory at given address.");
+    ("-cycle-limit", Arg.Set_int (Sail_lib.opt_cycle_limit), "<int> exit after given number of instructions executed.")]
 
 let usage_msg = "Sail OCaml RTS options:"
-let () = Arg.parse options (fun s -> opt_file_arguments := !opt_file_arguments @ [s]) usage_msg
+
+let () =
+  Arg.parse options (fun s -> opt_file_arguments := !opt_file_arguments @ [s]) usage_msg
 
 let rec load_raw_files = function
   | (file, addr) :: files -> begin
@@ -100,18 +93,19 @@ let rec load_raw_files = function
         while true do
           let b = input_byte ic in
           Sail_lib.wram !addr' b;
-          addr' := Nat_big_num.succ !addr'
+          addr' := Nat_big_num.succ !addr';
         done
-      with End_of_file ->
-        ();
-        load_raw_files files
+      with End_of_file -> ();
+      load_raw_files files
     end
   | [] -> ()
 
 let () =
   Random.self_init ();
   begin
-    match !opt_file_arguments with f :: _ -> load_elf f | _ -> ()
+    match !opt_file_arguments with
+    | f :: _ -> load_elf f
+    | _ -> ()
   end;
-  load_raw_files !opt_raw_files
-(* ocaml_backend.ml will append from here *)
+  load_raw_files !opt_raw_files;
+  (* ocaml_backend.ml will append from here *)
