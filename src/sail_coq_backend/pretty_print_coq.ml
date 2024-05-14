c8392b352d21ee14a10834b2fcb08f9cf6ddcf48
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-05-14 17:37:26 +0100

    Coq: update concurrency interface instantiation

--- src/sail_coq_backend/pretty_print_coq.ml
+++ src/sail_coq_backend/pretty_print_coq.ml
@@ -3832,12 +3832,10 @@ let pp_ast_coq library_style (types_file, types_modules) (defs_file, defs_module
                string "Definition read_reg {A E} := @read_reg register A E.";
                string "Definition write_reg {A E} := @write_reg register A E.";
                empty;
-               (* TODO: update once we've revised the interface. *)
                string "Module Arch <: Arch.";
-               string "  Definition reg := string.";
+               string "  Definition reg := register.";
                (* string "  Definition reg_eq : EqDecision reg := _.";
                   string "  Definition reg_countable : Countable reg := _.";*)
-               string "  Definition reg_type := register_value.";
                string "  Definition va_size := 64%N.";
                (* FIXME *)
                string "  Definition pa := " ^^ pp_typ params.pa_type ^^ string ".";
@@ -3850,7 +3848,6 @@ let pp_ast_coq library_style (types_file, types_modules) (defs_file, defs_module
                string "  Definition cache_op := " ^^ pp_typ params.cache_op_type ^^ string ".";
                string "  Definition tlb_op := " ^^ pp_typ params.tlbi_type ^^ string ".";
                string "  Definition fault (deps : Type) := " ^^ pp_typ params.fault_type ^^ string ".";
-               string "  Definition footprint_system_registers : list reg := [].";
                string "End Arch.";
                empty;
                string "Module Interface := Interface Arch.";
