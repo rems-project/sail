1c2c44103ea998e64a9a87772d902442b471d087
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-07-29 11:21:03 +0100

    Coq: fix up tests - cope with registers whose name overlaps with variables - misplaced rule for printing "nat" type - uniform naming of register references - fix initialisation in builtins and c tests

--- test/builtins/test.v
+++ test/builtins/test.v
@@ -4,7 +4,7 @@ Require Import List.
 Import ListNotations.
 
 Goal True.
-let result := eval cbv in (liftState register_accessors (main tt) (init_state tt) default_choice) in
+let result := eval cbv in (liftState register_accessors (main tt) (init_state init_regstate) default_choice) in
 match result with
   | [(Value tt,_,_)] => idtac "OK"
   | [(Ex (Failure ?s),_,_)] => idtac "Fail:" s
