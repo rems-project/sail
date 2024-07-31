0c0fc9dc5f7571589f30732764f3bb0fbe411273
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-07-31 17:25:38 +0100

    Coq: add stdpp Countable instances; related fixes - include transitive dependencies when only generating decidable equality   for types that use generic_eq - handle user-defined names for enumeration functions properly - don't attempt to use coq-record-update for empty records - use correct class for register value decidable eq in stdpp mode - use stdpp and coq-record-update in testing

--- test/builtins/test.v
+++ test/builtins/test.v
@@ -1,10 +1,10 @@
-From Sail Require Import State_monad State_lifting.
+From SailStdpp Require Import State_monad State_lifting.
 Require Import String.
 Require Import List.
 Import ListNotations.
 
 Goal True.
-let result := eval cbv in (liftState register_accessors (main tt) (init_state init_regstate) default_choice) in
+let result := eval vm_compute in (liftState register_accessors (main tt) (init_state init_regstate) default_choice) in
 match result with
   | [(Value tt,_,_)] => idtac "OK"
   | [(Ex (Failure ?s),_,_)] => idtac "Fail:" s
