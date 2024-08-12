ed2a36dbe8d0e5b8938e21e43de8db188acf9372
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-08-12 10:41:08 +0100

    Formatting of test filter

--- test/c/filter.ml
+++ test/c/filter.ml
@@ -1,21 +1,13 @@
-match input_char stdin with
-| '"' -> ()
-| _ -> exit 1
-;;
+match input_char stdin with '"' -> () | _ -> exit 1;;
 
 while true do
   match input_char stdin with
   | '"' -> begin
-    match input_char stdin with
-    | '"' -> output_char stdout '\"'
-    | '\n' ->
-      begin match input_char stdin with
-        | exception End_of_file -> exit 0
-        | _ -> exit 1
-      end
-    | exception End_of_file -> exit 0
-    | _ -> exit 1
-  end
+      match input_char stdin with
+      | '"' -> output_char stdout '\"'
+      | '\n' -> begin match input_char stdin with exception End_of_file -> exit 0 | _ -> exit 1 end
+      | exception End_of_file -> exit 0
+      | _ -> exit 1
+    end
   | c -> output_char stdout c
 done
-;;
