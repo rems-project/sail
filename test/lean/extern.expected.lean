104847aefafc214deb93d167e0da9f422670a5b7
Tobias Grosser <tobias@grosser.es>
2024-10-30 13:51:10 -0500

    lean: add support for external calls to [Int.add|sub|tdiv|tmod|tmod_positive]

--- test/lean/extern.expected.lean
+++ test/lean/extern.expected.lean
@@ -1,3 +1,18 @@
+def extern_add : Int :=
+  (Int.add 5 4)
+
+def extern_sub : Int :=
+  (Int.sub 5 4)
+
+def extern_tdiv : Int :=
+  (Int.tdiv 5 4)
+
+def extern_tmod : Int :=
+  (Int.tmod 5 4)
+
+def extern_tmod_positive : Int :=
+  (Int.tmod 5 4)
+
 def extern_negate : Int :=
   (Int.neg 5)
 
