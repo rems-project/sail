d9331ad40b9e23513d7135749816d826140a3860
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-11-21 15:12:33 +0000

    expected output for the test file

---
+++ test/lean/bitvec_operation.expected.lean
@@ -0,0 +1,32 @@
+def bitvector_eq (x : BitVec 16) (y : BitVec 16) : Bool :=
+  (Eq x y)
+
+def bitvector_neq (x : BitVec 16) (y : BitVec 16) : Bool :=
+  (Ne x y)
+
+def bitvector_trunc (x : BitVec 16) : BitVec 8 :=
+  (BitVec.truncate 8 x)
+
+def bitvector_append (x : BitVec 16) (y : BitVec 16) : BitVec 32 :=
+  (BitVec.append x y)
+
+def bitvector_add (x : BitVec 16) (y : BitVec 16) : BitVec 16 :=
+  (HAdd.hAdd x y)
+
+def bitvector_sub (x : BitVec 16) (y : BitVec 16) : BitVec 16 :=
+  (HSub.hSub x y)
+
+def bitvector_not (x : BitVec 16) : BitVec 16 :=
+  (Complement.complement x)
+
+def bitvector_and (x : BitVec 16) (y : BitVec 16) : BitVec 16 :=
+  (HAnd.hAnd x y)
+
+def bitvector_or (x : BitVec 16) (y : BitVec 16) : BitVec 16 :=
+  (HOr.hOr x y)
+
+def bitvector_xor (x : BitVec 16) (y : BitVec 16) : BitVec 16 :=
+  (HXor.hXor x y)
+
+def initialize_registers : Unit :=
+  ()
