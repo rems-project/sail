f5cb97e540f868bee71c8b42c23c8b659dbdd78b
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-11-22 16:11:54 +0000

    Fixed the test case that got broken

--- test/lean/extern_bitvec.expected.lean
+++ test/lean/extern_bitvec.expected.lean
@@ -2,8 +2,7 @@ def extern_const : BitVec 64 :=
   (0xFFFF000012340000 : BitVec 64)
 
 def extern_add : BitVec 16 :=
-  (Add.add (0xFFFF : BitVec 16) (0x1234 : BitVec 16))
+  (HAdd.hAdd (0xFFFF : BitVec 16) (0x1234 : BitVec 16))
 
 def initialize_registers : Unit :=
   ()
-
