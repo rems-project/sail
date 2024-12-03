1ada9216b577aacaec46536d5b4c38eae3344dcd
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-12-03 14:44:27 +0000

    formatting

--- src/sail_lean_backend/pretty_print_lean.ml
+++ src/sail_lean_backend/pretty_print_lean.ml
@@ -82,14 +82,13 @@ let rec doc_typ (Typ_aux (t, _) as typ) =
   | Typ_id (Id_aux (Id id, _)) -> string id
   | _ -> failwith "Type not translatable yet."
 
-
 let doc_typ_id (typ, fid) = concat [doc_id_ctor fid; space; colon; space; doc_typ typ; hardline]
 
 let doc_typ_quant tq =
   match tq with
-  | TypQ_tq qs -> (match qs with 
-    | [] -> string ""
-    | _ -> failwith "Type quantifier not translatable yet.")
+  | TypQ_tq qs -> (
+      match qs with [] -> string "" | _ -> failwith "Type quantifier not translatable yet."
+    )
   | TypQ_no_forall -> string ""
 
 let lean_escape_string s = Str.global_replace (Str.regexp "\"") "\"\"" s
@@ -110,7 +109,7 @@ let doc_lit (L_aux (lit, l)) =
   | L_string s -> utf8string ("\"" ^ lean_escape_string s ^ "\"")
   | L_real s -> utf8string s (* TODO test if this is really working *)
 
-let fail_doc_exp (e: tannot exp_aux) =
+let fail_doc_exp (e : tannot exp_aux) =
   match e with
   | E_block _ -> failwith "Expression E_block not translatable yet"
   | E_id _ -> failwith "Expression E_id not translatable yet"
@@ -240,13 +239,11 @@ let doc_typdef (TD_aux (td, tannot) as full_typdef) =
         ^^ enums_doc ^^ hardline ^^ string "deriving" ^^ space
         ^^ separate (comma ^^ space) derivers
         )
-  | TD_record (Id_aux (Id id, _), TypQ_aux (tq, _), fields, _) -> 
-    let fields = List.map doc_typ_id fields in
-    let enums_doc = concat fields in
-    let rectyp = doc_typ_quant tq in
-    nest 2
-     (flow (break 1) [string "structure"; string id; rectyp; string "where"]
-     ^^ enums_doc)
+  | TD_record (Id_aux (Id id, _), TypQ_aux (tq, _), fields, _) ->
+      let fields = List.map doc_typ_id fields in
+      let enums_doc = concat fields in
+      let rectyp = doc_typ_quant tq in
+      nest 2 (flow (break 1) [string "structure"; string id; rectyp; string "where"] ^^ enums_doc)
   | _ -> fail_doc_typdef td
 
 let doc_def (DEF_aux (aux, def_annot) as def) =
