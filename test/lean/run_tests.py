192e919e7e4db1f5ed6ca382e3cc8a9606093491
Raito Bezarius <masterancpp@gmail.com>
2025-01-13 15:32:13 +0100

    fix(test/lean/run_tests): ensure the directory exist, do not fail with EEXIST
    If the directory already exist, it's fine, no need to fail with EEXIST.
    
    This fixes the situation where the bug already exist.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- test/lean/run_tests.py
+++ test/lean/run_tests.py
@@ -26,7 +26,7 @@ def test_lean():
             basename = os.path.splitext(os.path.basename(filename))[0]
             tests[filename] = os.fork()
             if tests[filename] == 0:
-                step('mkdir {}'.format(basename))
+                step('mkdir -p {}'.format(basename))
                 step('\'{}\' {} --lean --lean-output-dir {}'.format(sail, filename, basename))
                 step('diff {}/out/Out.lean {}.expected.lean'.format(basename, basename))
                 step('rm -r {}'.format(basename))
