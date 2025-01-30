7f8a37ae23830c145ffc45e945537c9b5108916a
Léon Frenot <leon.frenot@ens-lyon.fr>
2025-01-30 11:02:55 +0000

    Editing enum test

--- test/lean/enum.expected.lean
+++ test/lean/enum.expected.lean
@@ -11,6 +11,19 @@ abbrev SailM := StateM Unit
 def undefined_E : SailM E := do
   sorry
 
+/-- Type quantifiers: arg_ : Int, 0 ≤ arg_ ∧ arg_ ≤ 2 -/
+def E_of_num (arg_ : Nat) : E :=
+  match arg_ with
+  | 0 => A
+  | 1 => B
+  | _ => C
+
+def num_of_E (arg_ : E) : Int :=
+  match arg_ with
+  | A => 0
+  | B => 1
+  | C => 2
+
 def initialize_registers : Unit :=
   ()
 
