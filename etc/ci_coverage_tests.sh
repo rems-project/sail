8a975427adcc3ee31a222e88c66cce415870f46e
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2025-02-13 15:00:29 +0000

    CI: Make CI run Sail test suites in parallel

--- etc/ci_coverage_tests.sh
+++ etc/ci_coverage_tests.sh
@@ -4,4 +4,25 @@ set -eu
 
 export TEST_PAR=4
 
-test/run_coverage_tests.sh
+returncode=0
+
+if [ "$1" = "typecheck" ]; then
+    test/typecheck/run_tests.py || returncode=1
+elif [ "$1" = "exec" ]; then
+    test/ocaml/run_tests.sh || returncode=1
+    test/c/run_tests.py || returncode=1
+elif [ "$1" = "sv" ]; then
+    test/sv/run_tests.py || returncode=1
+elif [ "$1" = "prover" ]; then
+    test/lem/run_tests.py || returncode=1
+    test/smt/run_tests.py || returncode=1
+    test/lean/run_tests.py || returncode=1
+elif [ "$1" = "other" ]; then
+    test/lexing/run_tests.py || returncode=1
+    test/pattern_completeness/run_tests.py || returncode=1
+    test/sailcov/run_tests.py || returncode=1
+    test/format/run_tests.py || returncode=1
+    test/oneoff/run_tests.py || returncode=1
+fi
+
+exit $returncode
