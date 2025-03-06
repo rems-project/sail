82cbcf668d777ef2098f8c08fc7c11ba8f8f8a0d
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2025-03-06 19:27:15 +0000

    Run the tests in CI
    Use dummy output for other targets for now

--- src/sail_c_backend/c_backend.ml
+++ src/sail_c_backend/c_backend.ml
@@ -2405,13 +2405,10 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
 
       let unit_tests = get_unit_tests cdefs in
 
-      (* TODO: Formatting here isn't quite right. What are the arguments to jump? *)
       let unit_test_functions =
         ["unit (*const SAIL_TESTS[])(unit) = {"]
-        @ Util.map_last
-            (fun is_last id -> sprintf "  %s%s" (sgen_function_id id) (if is_last then "" else ","))
-            unit_tests
-        @ ["};"]
+        @ List.map (fun id -> sprintf "  %s," (sgen_function_id id)) unit_tests
+        @ ["  NULL"; "};"]
         |> separate_map hardline string
       in
 
@@ -2429,9 +2426,9 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
          in your own custom test runner. *)
       let model_test =
         [
-          Printf.sprintf "%svoid model_test()" (static ());
+          Printf.sprintf "%svoid model_test(void)" (static ());
           "{";
-          "  for (size_t i = 0; SAIL_TESTS[i] != 0 && SAIL_TEST_NAMES[i] != 0; ++i) {";
+          "  for (size_t i = 0; SAIL_TESTS[i] != NULL; ++i) {";
           "    model_init();";
           "    printf(\"Testing %s\\n\", SAIL_TEST_NAMES[i]);";
           "    SAIL_TESTS[i](UNIT);";
@@ -2489,7 +2486,7 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
                  model_init ^^ hlhl ^^ model_fini ^^ hlhl ^^ model_pre_exit ^^ hlhl ^^ model_main ^^ hlhl
                else empty
              )
-          ^^ actual_main ^^ hlhl ^^ unit_test_functions ^^ hlhl ^^ unit_test_names ^^ hlhl ^^ model_test ^^ hardline
+          ^^ unit_test_functions ^^ hlhl ^^ unit_test_names ^^ hlhl ^^ model_test ^^ hlhl ^^ actual_main ^^ hardline
           ^^ end_extern_cpp ^^ hardline
           )
       )
