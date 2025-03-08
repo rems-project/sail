3132137024da5a0c7d3ad001e772f7af99dda7f4
Tobias Grosser <tobias@grosser.es>
2025-03-08 05:51:18 +0000

    Lean: join2 uses big endian
    This fixes the bv_literal test case.

--- test/lean/run_tests.py
+++ test/lean/run_tests.py
@@ -23,7 +23,6 @@ sail = get_sail()
 # Not all self-tests are supported.
 skip_selftests = {
     'list_rec_functions2',
-    'bv_literal',
     'pow2_var',
     'large_bitvector',
     'exn_hello_world',
