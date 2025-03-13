14d2eaa60bdd06f7a04177f19e5ee69c1798b3d2
James Parker <james@galois.com>
2025-03-13 11:58:56 -0400

    Initialize Lean testing for RISC-V tests

---
+++ test/lean-riscv/LeanRiscv.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `LeanRiscv` library.
+-- Import modules here that should be built as part of the library.
+import ELFSage
+import LeanRiscv.Basic
---
+++ test/lean-riscv/Main.lean
@@ -0,0 +1,4 @@
+import LeanRiscv
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
