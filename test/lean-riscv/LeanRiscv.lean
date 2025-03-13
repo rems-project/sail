159ec8421bfcb6992b79528ea352499a805ee4b2
James Parker <james@galois.com>
2025-03-13 14:19:25 -0400

    Parse an elf file

--- test/lean-riscv/LeanRiscv.lean
+++ test/lean-riscv/LeanRiscv.lean
@@ -1,4 +1,15 @@
 -- This module serves as the root of the `LeanRiscv` library.
 -- Import modules here that should be built as part of the library.
 import ELFSage
-import LeanRiscv.Basic
+
+def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := do
+  let bytes <- IO.FS.readBinFile elfFilepath
+  match mkRawELFFile? bytes with
+  | .error warning => do
+    pure (.error warning)
+  | .ok (.elf32 elf) => do
+    -- sorry
+    IO.println s!"{repr elf}"
+    pure (.ok elf)
+  | .ok (.elf64 elf) => do
+    pure (.error "64 bit ELF file not supported")
--- test/lean-riscv/Main.lean
+++ test/lean-riscv/Main.lean
@@ -1,4 +1,15 @@
 import LeanRiscv
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main (args : List String) : IO UInt32 := do
+  if args.length != 2 then do
+    IO.println "usage: run-riscv-lean <elf_file>"
+
+    pure 255
+  else do
+    -- Parse input elf file.
+    let elfF <- readElf32 args[1]!
+    
+    pure 0
+    -- TODO:
+    -- Convert to Sail's AST
+    -- Run program
