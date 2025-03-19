053d96fe80f253f6b007c244fc6c6a090ed53170
James Parker <james@galois.com>
2025-03-18 22:36:29 -0400

    Imports working

--- test/lean-riscv/LeanRiscv.lean
+++ test/lean-riscv/LeanRiscv.lean
@@ -1,7 +1,7 @@
 -- This module serves as the root of the `LeanRiscv` library.
 -- Import modules here that should be built as part of the library.
 import ELFSage
--- import LeanRV64DLEAN
+import LeanRV64DLEAN
 import LeanRV64DLEAN.Sail.Sail
 
 def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := do
@@ -17,14 +17,12 @@ def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := 
     pure (.error "64 bit ELF file not supported")
 
 
-#check mkRawELFFile?
--- #check sail_model_init
-
--- open LeanRV64DLEAN.Sail.Sail
-def runElf32 (elf : EF32File) : IO PUnit := do
-    -- IO.println "TODO"
-    let _ := sail_model_init
-    main_of_sail_main ⟨default, (), default, default, default, default⟩ (sail_model_init >=> sail_main)
+def runElf32 (elf : EF32File) : IO UInt32 :=
+  open Sail in
+  open Functions in
+  do
+  -- IO.println "TODO"
+  main_of_sail_main ⟨default, (), default, default, default, default⟩ (sail_model_init >=> sail_main)
 
 
   
