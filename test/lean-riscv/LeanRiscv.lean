07a92551d848765bee957a6acc7ee476a48bdd87
James Parker <james@galois.com>
2025-03-18 22:21:28 -0400

    Attempt to add and use RISC-V model as dependency

--- test/lean-riscv/LeanRiscv.lean
+++ test/lean-riscv/LeanRiscv.lean
@@ -1,6 +1,8 @@
 -- This module serves as the root of the `LeanRiscv` library.
 -- Import modules here that should be built as part of the library.
 import ELFSage
+-- import LeanRV64DLEAN
+import LeanRV64DLEAN.Sail.Sail
 
 def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := do
   let bytes <- IO.FS.readBinFile elfFilepath
@@ -13,3 +15,17 @@ def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := 
     pure (.ok elf)
   | .ok (.elf64 elf) => do
     pure (.error "64 bit ELF file not supported")
+
+
+#check mkRawELFFile?
+-- #check sail_model_init
+
+-- open LeanRV64DLEAN.Sail.Sail
+def runElf32 (elf : EF32File) : IO PUnit := do
+    -- IO.println "TODO"
+    let _ := sail_model_init
+    main_of_sail_main ⟨default, (), default, default, default, default⟩ (sail_model_init >=> sail_main)
+
+
+  
+
--- test/lean-riscv/Main.lean
+++ test/lean-riscv/Main.lean
@@ -8,8 +8,11 @@ def main (args : List String) : IO UInt32 := do
   else do
     -- Parse input elf file.
     let elfF <- readElf32 args[1]!
+
+    -- TODO:
+    -- Run program
+    runElf32 elfF
+
+
     
     pure 0
-    -- TODO:
-    -- Convert to Sail's AST
-    -- Run program
