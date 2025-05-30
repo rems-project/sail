201341fbc23d883d7728574ba837e181669228e7
jn80842 <newcomb@galois.com>
2025-05-29 19:39:30 -0700

    point lean emulator at current sail-riscv-lean build

--- test/lean-riscv/LeanRiscv.lean
+++ test/lean-riscv/LeanRiscv.lean
@@ -1,8 +1,8 @@
 -- This module serves as the root of the `LeanRiscv` library.
 -- Import modules here that should be built as part of the library.
 import ELFSage
-import LeanRV64DLEAN
-import LeanRV64DLEAN.Sail.Sail
+import LeanRV64D
+import LeanRV64D.Sail.Sail
 
 def readElf32 (elfFilepath : System.FilePath) : IO (Except String ELF32File) := do
   let bytes <- IO.FS.readBinFile elfFilepath
@@ -45,7 +45,7 @@ def initializeMemory (_size: MachineBits) (elf : ELF32File) : Std.HashMap Nat (B
 
   let update_mem_segment mem first_addr body :=
     let addrs := Array.range' first_addr (Array.size body)
-    Array.foldl (λ mem (addr, byte) => 
+    Array.foldl (λ mem (addr, byte) =>
       if mem.contains addr then
         panic s!"Address {addr} is already written to!"
       else
@@ -53,7 +53,7 @@ def initializeMemory (_size: MachineBits) (elf : ELF32File) : Std.HashMap Nat (B
     ) mem (Array.zip addrs body)
 
   -- Handle interpreted_segments
-  let mem'' := List.foldl (λ mem (_header, inst) => 
+  let mem'' := List.foldl (λ mem (_header, inst) =>
           -- TODO(JP): Is this address correct?
           update_mem_segment mem inst.segment_base inst.segment_body.data
         ) default elf.interpreted_segments
@@ -70,12 +70,12 @@ def initializeMemory (_size: MachineBits) (elf : ELF32File) : Std.HashMap Nat (B
 
   mem
 
-def initializeRegisters : Std.DHashMap Register RegisterType :=
+def initializeRegisters : Std.ExtDHashMap Register RegisterType :=
   -- TODO: initialize register properly
-  Std.DHashMap.empty
+  Std.ExtDHashMap.emptyWithCapacity
 
 def my_main (_ : PUnit) :=
-  open LeanRV64DLEAN.Functions in
+  open LeanRV64D.Functions in
   open Sail in
   do
   -- monadLift (IO.print "TEST")
@@ -87,11 +87,9 @@ def my_main (_ : PUnit) :=
 
 def runElf32 (elf : ELF32File) : IO UInt32 :=
   open Sail in
-  open LeanRV64DLEAN.Functions in
+  open LeanRV64D.Functions in
   let mem := initializeMemory MachineBits.B32 elf
   let regs := initializeRegisters
   let initialState := ⟨regs, (), mem, default, default, default⟩
   main_of_sail_main initialState (sail_model_init >=> my_main)
   -- main_of_sail_main initialState my_main
-  
-
