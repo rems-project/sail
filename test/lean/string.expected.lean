90cbbfc2a14d3e213d19f804890ea25f667d3475
Léo Stefanesco <leo.lveb@gmail.com>
2025-05-30 14:40:09 +0100

    Lean: Fix escape in strings

---
+++ test/lean/string.expected.lean
@@ -0,0 +1,95 @@
+import Out.Sail.Sail
+import Out.Sail.BitVec
+
+open PreSail
+
+set_option maxHeartbeats 1_000_000_000
+set_option maxRecDepth 1_000_000
+set_option linter.unusedVariables false
+set_option match.ignoreUnusedAlts true
+
+open Sail
+
+abbrev Register := PEmpty
+abbrev RegisterType : Register -> Type := PEmpty.elim
+
+abbrev exception := Unit
+
+abbrev SailM := PreSailM RegisterType trivialChoiceSource exception
+
+
+XXXXXXXXX
+
+import Out.Sail.Sail
+import Out.Sail.BitVec
+import Out.Sail.IntRange
+import Out.Defs
+import Out.Specialization
+import Out.FakeReal
+
+set_option maxHeartbeats 1_000_000_000
+set_option maxRecDepth 1_000_000
+set_option linter.unusedVariables false
+set_option match.ignoreUnusedAlts true
+
+open Sail
+
+namespace Out.Functions
+
+/-- Type quantifiers: k_ex448# : Bool, k_ex447# : Bool -/
+def neq_bool (x : Bool) (y : Bool) : Bool :=
+  (! (x == y))
+
+/-- Type quantifiers: x : Int -/
+def __id (x : Int) : Int :=
+  x
+
+/-- Type quantifiers: n : Int, m : Int -/
+def _shl_int_general (m : Int) (n : Int) : Int :=
+  bif (n ≥b 0)
+  then (Int.shiftl m n)
+  else (Int.shiftr m (Neg.neg n))
+
+/-- Type quantifiers: n : Int, m : Int -/
+def _shr_int_general (m : Int) (n : Int) : Int :=
+  bif (n ≥b 0)
+  then (Int.shiftr m n)
+  else (Int.shiftl m (Neg.neg n))
+
+/-- Type quantifiers: m : Int, n : Int -/
+def fdiv_int (n : Int) (m : Int) : Int :=
+  bif ((n <b 0) && (m >b 0))
+  then ((Int.tdiv (n +i 1) m) -i 1)
+  else
+    (bif ((n >b 0) && (m <b 0))
+    then ((Int.tdiv (n -i 1) m) -i 1)
+    else (Int.tdiv n m))
+
+/-- Type quantifiers: m : Int, n : Int -/
+def fmod_int (n : Int) (m : Int) : Int :=
+  (n -i (m *i (fdiv_int n m)))
+
+/-- Type quantifiers: k_n : Int -/
+def concat_str_bits (str : String) (x : (BitVec k_n)) : String :=
+  (HAppend.hAppend str (BitVec.toFormatted x))
+
+/-- Type quantifiers: x : Int -/
+def concat_str_dec (str : String) (x : Int) : String :=
+  (HAppend.hAppend str (Int.repr x))
+
+def multi_line (_ : Unit) : Unit :=
+  let _ : Unit := (print_endline "Hello, World!")
+  (print_endline "Hello, World!")
+
+def test (_ : Unit) : Unit :=
+  let _ : Unit := (print_endline "'")
+  let _ : Unit := (print_endline "\"")
+  (print_endline "'")
+
+def initialize_registers (_ : Unit) : Unit :=
+  ()
+
+def sail_model_init (x_0 : Unit) : Unit :=
+  (initialize_registers ())
+
+end Out.Functions
