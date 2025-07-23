3c6c8c3f4d7104153fd4269a8818ed5add6ae693
Brian Campbell <Brian.Campbell@ed.ac.uk>
2025-07-23 11:59:05 +0100

    Rocq: fix for type variables fixed by an equation in function sigs
    Ensures that equations like `'len = 'm * 'n` appearing in type signatures
    are used properly.  There's an example in test/c/vector_example.sail.

--- test/c/run_tests.py
+++ test/c/run_tests.py
@@ -154,7 +154,6 @@ def test_coq(name):
     results.expect_failure("real_prop.sail", "random_real not available for Coq at present")
     results.expect_failure("fail_assert_mono_bug.sail", "test output checking not supported for Coq yet")
     results.expect_failure("fail_issue203.sail", "test output checking not supported for Coq yet")
-    results.expect_failure("vector_example.sail", "bug: function defs and function calls treat 'len equation differently in Coq backend")
     results.expect_failure("list_torture.sail", "Coq backend doesn't remove a phantom type parameter")
     results.expect_failure("lib_hex_bits_signed.sail","bug: unable to drop the type variable")
     results.expect_failure("for_shadow.sail","bug: remove_e_assign rewrite assumes <= available")
