b9b3411cbb2e742479a9b9b56dc8a4bbd1c7c6b5
Tim Hutt <timothy.hutt@codasip.com>
2025-10-17 13:13:42 +0100

    Change default z3_problems filename to sail_smt_cache
    This makes it a little more obvious what the file is.
    
    Fixes #1449

--- test/oneoff/bad_cache/test.sh
+++ test/oneoff/bad_cache/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -f bad_cache.result z3_problems
-echo -n -e 7fcba3a13c51ab2786c5b710e80c5f88\\x05 > z3_problems
+rm -f bad_cache.result sail_smt_cache
+echo -n -e 7fcba3a13c51ab2786c5b710e80c5f88\\x05 > sail_smt_cache
 sail --memo-z3 2> bad_cache.result || true
 diff bad_cache.result bad_cache.expect
