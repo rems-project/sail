010b8de47856e769146dad196fb093e3cf0b54e4
Tim Hutt <tdhutt@gmail.com>
2025-12-02 21:56:52 +0000

    Support Windows compilation by disabling the REPL on Windows
    This also currently includes some incidental CI improvements that I made when trying to punch Github CI in the face until it worked:
    
    * Use explicit `os` versions
    * Use `runner.os ==` instead of `startswith(matrix.os`
    * Use the `setup-z3` action to set up Z3
    * Use the `setup-ocaml` action. This includes caching.
    
    Also some of the tarball generation logic is moved from the Makefile into `sail_maker` (OCaml) because it's difficult to make portable Makefiles.

--- src/sail_maker/sail_maker.ml
+++ src/sail_maker/sail_maker.ml
@@ -46,10 +46,6 @@
 
 open Printf
 
-let opt_gen_manifest = ref false
-
-let options = Arg.align [("-gen_manifest", Arg.Set opt_gen_manifest, "generate manifest.ml")]
-
 let git_command args =
   try
     let git_out, git_in, git_err = Unix.open_process_full ("git " ^ args) (Unix.environment ()) in
@@ -64,10 +60,108 @@ let gen_manifest () =
   ksprintf print_endline "let branch = \"%s\""
     (Option.value (git_command "rev-parse --abbrev-ref HEAD") ~default:"unknown branch")
 
-let usage = "sail_manifest <options>"
+(* Copy a single file. Surprisingly there's no built-in function for this. *)
+let copy_file (input_path : string) (output_path : string) =
+  let buffer_size = 8192 in
+  let buffer = Bytes.create buffer_size in
+  let ic = open_in_bin input_path in
+  let oc = open_out_bin output_path in
+  try
+    let rec copy_loop () =
+      let bytes_read = input ic buffer 0 buffer_size in
+      if bytes_read > 0 then begin
+        output oc buffer 0 bytes_read;
+        copy_loop ()
+      end
+    in
+    copy_loop ();
+    close_in ic;
+    close_out oc
+  with e ->
+    close_in_noerr ic;
+    close_out_noerr oc;
+    raise e
+
+(* Recursively remove a file or directory tree. *)
+let rec remove_file_or_dir path =
+  if Sys.file_exists path then
+    if Sys.is_directory path then begin
+      (* Remove all contents of the directory *)
+      let entries = Sys.readdir path in
+      Array.iter
+        (fun entry ->
+          let full_path = Filename.concat path entry in
+          remove_file_or_dir full_path
+        )
+        entries;
+      (* Remove the now-empty directory *)
+      Unix.rmdir path
+    end
+    else
+      (* Remove regular file *)
+      Sys.remove path
+
+let parse_tarball_args (args : string list) =
+  let usage_msg = "sail_maker tarball --prefix=PREFIX [--z3=Z3_EXE_PATH] [--gmp=GMP_DLL_PATH]" in
+  let prefix = ref "" in
+  let z3_path = ref "" in
+  let gmp_path = ref "" in
+
+  let speclist =
+    [
+      ("--prefix", Arg.Set_string prefix, "Path to install prefix");
+      ("--z3", Arg.Set_string z3_path, "Set path to z3 executable");
+      ("--gmp", Arg.Set_string gmp_path, "Set path to GMP shared library");
+    ]
+  in
+
+  (* Positional arguments are unused and will cause an error. *)
+  let anon_fun _ = () in
+
+  let args = Array.of_list ("sail_maker" :: args) in
+
+  let () = Arg.parse_argv args speclist anon_fun usage_msg in
+
+  (!prefix, !z3_path, !gmp_path)
+
+let tarball (prefix : string) (z3 : string) (gmp : string) =
+  let bindir = Filename.concat prefix "bin" in
+  (* This contains a load of OCaml source files we don't need in the binary distribution. *)
+  remove_file_or_dir (Filename.concat prefix "lib");
+  (* The sail_maker binary gets installed and I'm not sure how to avoid that with Dune
+  so just delete it now. *)
+  remove_file_or_dir (Filename.concat bindir (if Sys.win32 then "sail_maker.exe" else "sail_maker"));
+  (* Copy in some extra license files. Maybe Dune could do this. *)
+  copy_file "LICENSE" (Filename.concat prefix "LICENSE");
+  copy_file "THIRD_PARTY_FILES.md" (Filename.concat prefix "THIRD_PARTY_FILES.md");
+  copy_file "etc/tarball_extra/INSTALL" (Filename.concat prefix "INSTALL");
+  copy_file "etc/tarball_extra/Z3_LICENSE" (Filename.concat prefix "Z3_LICENSE");
+  (* Copy in precompiled coverage library. *)
+  let coverage_libname = if Sys.win32 then "sail_coverage.lib" else "libsail_coverage.a" in
+  copy_file
+    (Filename.concat "lib/coverage/target/release" coverage_libname)
+    (Filename.concat (Filename.concat prefix "share/sail/lib/coverage") coverage_libname);
+  (* For convenience, copy in a z3 executable and GMP shared library (Windows only). *)
+  if z3 <> "" then copy_file z3 (Filename.concat bindir (Filename.basename z3));
+  if gmp <> "" then copy_file gmp (Filename.concat bindir (Filename.basename gmp));
+  ()
+
+let usage =
+  "sail_maker gen_manifest\n\n\
+  \  Write manifest.ml to stdout containing Git commit and branch information.\n\n\n\n\
+   sail_maker tarball --prefix=PREFIX [--z3=Z3_EXE_PATH] [--gmp=GMP_DLL_PATH]\n\n\
+  \  Used for fixing up the `dune install` output in preparation for making release tarballs.\n\n"
 
 let main () =
-  Arg.parse options (fun _ -> ()) usage;
-  if !opt_gen_manifest then gen_manifest ()
+  (* OCaml's pattern matching support is not very good for arrays so convert
+  to a list. OCaml loves lists more than ones and zeros. *)
+  match Array.to_list Sys.argv with
+  | [_; "gen_manifest"] -> gen_manifest ()
+  | _ :: "tarball" :: args ->
+      let prefix, z3_path, gmp_path = parse_tarball_args args in
+      tarball prefix z3_path gmp_path
+  | _ ->
+      prerr_endline usage;
+      exit 1
 
 let () = main ()
