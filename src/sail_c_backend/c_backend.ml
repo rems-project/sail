8262c3c27f98bb00e58dd7a43324361bd7d850b7
Tim Hutt <tdhutt@gmail.com>
2026-02-10 18:17:03 +0000

    Avoid generating some empty switches in C code (#1618)
    * Avoid generating some empty switches in C code
    
    This avoids some empty `switch` outputs, of which there were a fair number.
    
    * Older OCaml compatability
    
    Co-authored-by: Alasdair Armstrong <alasdair.armstrong@cl.cam.ac.uk>

--- src/sail_c_backend/c_backend.ml
+++ src/sail_c_backend/c_backend.ml
@@ -1735,14 +1735,16 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
         in
         (* Create a switch that does something for each constructor *)
         let each_ctor v f ctors =
-          c_switch (ksprintf string "(%skind)" v)
-            (List.map
-               (fun (ctor_id, ctyp) ->
-                 let enum_label = ksprintf string "Kind_%s" (sgen_id ctor_id) in
-                 (enum_label, match f ctor_id ctyp with Some op -> [op] | None -> [])
-               )
-               ctors
-            )
+          let cases =
+            List.filter_map
+              (fun (ctor_id, ctyp) ->
+                Option.map (fun op -> (ksprintf string "Kind_%s" (sgen_id ctor_id), [op])) (f ctor_id ctyp)
+              )
+              ctors
+          in
+          (* Avoid outputting empty switches. This is here instead of in `c_switch` because
+            in `c_switch` we don't know that the condition expression has no side effects. *)
+          if cases = [] then empty else c_switch (ksprintf string "(%skind)" v) cases
         in
         let codegen_init =
           let n = sgen_id id in
