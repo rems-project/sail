2335678b66ff842f03fca5a3519e8679ed05992d
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2026-03-09 08:38:01 +0000

    Fix project suite

--- test/suites/project.py
+++ test/suites/project.py
@@ -12,13 +12,25 @@ _FAILURE_DIR = os.path.join(_SUITE_DIR, "failure")
 class ProjectTests(SailTest):
     def run(self):
         self.banner("Testing project")
-        self.run_tests("project", Batcher(_FAILURE_DIR), self._test, testdir=_SUITE_DIR)
+        self.run_tests("project", Batcher(_FAILURE_DIR), self._test)
 
     def _test(self, test):
+        os.chdir(_SUITE_DIR)
         step(
-            f"'{self.sail}' failure/{test.filename} 2> failure/{test.basename}.error",
+            f"'{self.sail}' failure/{test.filename} 2> {self.work_dir}/{test.basename}.error",
             expected_status=1,
             env={"SAIL_NEW_CLI": "true"},
         )
-        step(f"diff failure/{test.basename}.expect failure/{test.basename}.error")
-        step(f"rm failure/{test.basename}.error")
+        status = step_with_status(
+            f"diff failure/{test.basename}.expect {self.work_dir}/{test.basename}.error"
+        )
+        if status != 0:
+            if args.update_expected:
+                print(f"Overriding file {test.basename}.expect")
+                step(
+                    f"'{self.sail}' failure/{test.filename} 2> failure/{test.basename}.expect",
+                    expected_status=1,
+                    env={"SAIL_NEW_CLI": "true"},
+                )
+            else:
+                sys.exit(1)
