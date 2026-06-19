1f8f173f12b1531a9a9f9c058408196c45b2df62
Brian Campbell <Brian.Campbell@ed.ac.uk>
2026-06-19 15:27:06 +0100

    Fix unsigned_subrange mono_rewrites definition, add tests
    See https://github.com/rems-project/sail-arm/issues/32

--- test/sv/run_tests.py
+++ test/sv/run_tests.py
@@ -37,6 +37,7 @@ skip_tests = {
     'concurrency_interface_v2',
     'concurrency_interface_v2_var',
     'mini_builtins', # unsupported builtins
+    'test_mono_rewrites', # verilator bug (in CI, works with latest)
 }
 
 print("Sail is {}".format(sail))
