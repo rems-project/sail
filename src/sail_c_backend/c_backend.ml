222645b9e520195727edc67fa88f173d6104d556
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2026-07-06 15:35:35 +0100

    Adjust enum type formatting slightly

--- src/sail_c_backend/c_backend.ml
+++ src/sail_c_backend/c_backend.ml
@@ -1664,8 +1664,10 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
           string (Printf.sprintf "static enum %s UNDEFINED(%s)(unit u) { return %s; }" name name (sgen_id first_id))
         in
         (* Conservatively use the smallest size of int to guard specifying storage size.  This assumes C++11,
-           but could also be done for C23.*)
-        let enum_type = if Config.cpp && List.length ids < 65536 then ": int" else "" in
+           but could also be done for C23. *)
+        let enum_type =
+          if Config.cpp && List.length ids < 65536 then space ^^ colon ^^ space ^^ string "int" else empty
+        in
         [
           TypeDeclaration
             (string (Printf.sprintf "// enum %s" (string_of_id id))
@@ -1673,8 +1675,7 @@ module Codegen (Config : CODEGEN_CONFIG) = struct
             ^^ separate space
                  [
                    string "enum";
-                   codegen_id id;
-                   string enum_type;
+                   codegen_id id ^^ enum_type;
                    lbrace;
                    separate_map (comma ^^ space) codegen_id ids;
                    rbrace ^^ semi;
